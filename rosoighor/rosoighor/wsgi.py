54a49994dacf9978e0da5cd08ddd860c43e9e8f8
Akib Arham Azan <azan161971@gmail.com>
2026-05-15 21:30:17 +0600

    Initial commit

---
+++ rosoighor/rosoighor/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for rosoighor project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rosoighor.settings')
+
+application = get_wsgi_application()
